9ed6f5fdc656d66575458575ab1564894ab1a1b2
Jason Kulatunga <jk17@ualberta.ca>
2016-11-11 18:09:11 -0800

    added external tripit api endpoint. tons of console.log statements.

--- handler.js
+++ handler.js
@@ -2,11 +2,16 @@
 require('dotenv').config();
 var link_controller = require("./lib/controllers/link");
 var test_controller = require("./lib/controllers/test");
+
+var external_tripit_controller = require('./lib/controllers/external/tripit')
+
 module.exports = {
 
     link_connect: link_controller.connect,
     link_callback: link_controller.callback,
 
-    test: test_controller.test
+    test: test_controller.test,
+
+    external_tripit_trips: external_tripit_controller.trips
 
 }
